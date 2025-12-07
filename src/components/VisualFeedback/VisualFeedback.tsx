0336932646fc5238efff8989d6419ccff25b64e7
= <brendanmiron845@gmail.com>
2025-12-07 10:56:33 -0500

    Fixing react error

--- src/components/VisualFeedback/VisualFeedback.tsx
+++ src/components/VisualFeedback/VisualFeedback.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { type DesignCanvas } from '../../types/game.types';
 import styles from './VisualFeedback.module.css';
 
